820957fd16adee2686821e81464e4001c114b6e4
王政 <wangz@jfinfo.com>
2025-11-28 15:49:17 +0800

    postgresql group by

---
+++ database/postgres/sql/console_1.sql
@@ -0,0 +1,67 @@
+select * from employees where id = 1;
+
+select array[1,2,3]::integer[];
+select name, (select e.core/2 from employees as e where e.id = employees.id) from employees;
+
+create table arr (f1 int[], f2 int[]);
+insert into arr values (array[[1,2], [3,4]], array[[5,6], [7,8]]);
+select array[f1, f2, '{{10,11}, {12,13}}'::int[]] from arr;
+SELECT ROW(1,2.5,'this is a test');
+
+select *, case
+            when core < 0 then 'bad'
+            when core < 60 then 'no good'
+            when core < 80 then 'great'
+            when core < 90 then 'good'
+            else 'very good'
+    end
+from employees order by core DESC;
+
+select *,
+case
+    when core >= 90 then 'high'
+    when core >= 80 then 'mid'
+    when core >= 70 then 'low'
+    else 'low low'
+end
+from employees order by
+case
+    when core >= 90 then 1
+    when core >= 80 then 2
+    when core >= 70 then 3
+    else 4
+end;
+
+-- with recursive 递归操作 向下查询
+
+with recursive menu_node as (
+    -- 非递归查询
+    select id, name, parent_id, sort, 1 as depth from menus where parent_id is null
+    union all
+    -- 递归查询
+    select s.id, s.name, s.parent_id, s.sort, m.depth + 1 from menus s join menu_node m on m.id = s.parent_id
+)
+select * from menu_node order by depth, sort desc;
+
+-- with recursive 递归操作 向上查询
+
+with recursive c_node as (
+    select * from menus where id = 8
+    union all
+    select m.* from menus m
+    inner join c_node c on c.parent_id = m.id
+)
+select string_agg(name, '/') as path from( select name from c_node order by id ) as cnn
+
+-- INSERT INTO menus (name, link) VALUES
+--                                    ('首页', '/'),
+--                                    ('关于我们', '/about'),
+--                                    ('服务', '/services');
+
+-- 假设上面的查询返回的ID为1, 2, 3（对应于“首页”, “关于我们”, “服务”）
+-- INSERT INTO menus (name, link, parent_id) VALUES
+--   ('首页 - 子项1', '/home-sub1', 1), -- 假设1是首页的ID
+--   ('关于我们 - 子项1', '/about-sub1', 2), -- 假设2是“关于我们”的ID
+--   ('服务 - 子项1', '/services-sub1', 3); -- 假设3是“服务”的ID
+
+
